5440bd236b5ec07053ec8622e8f7d56956fd1c73
Tangqiketang <641718840@qq.com>
2022-09-18 23:50:43 +0800

    基础

---
+++ 1.基础/条件语句.py
@@ -0,0 +1,20 @@
+#########if elif else
+name = "wm"
+if name == 'wm':
+    print("wm---yye")
+elif name == "yy":
+    print("yyyy")
+else:
+    print("xxxxx")
+
+
+##########for循环
+for x in "abcd":
+    print(x)
+###利用角标
+list1=["a","b","c","d"]
+for i in range(len(list1)):
+    print(list1[i])
+
+
+
