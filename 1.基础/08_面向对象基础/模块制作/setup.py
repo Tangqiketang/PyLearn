ecf15649f2a770dcdf7d07295b56a38523ecb067
wangmin <641718840@qq.com>
2025-02-05 18:59:44 +0800

    包安装移动

--- 1.基础/08_面向对象基础/模块制作/setup.py
+++ 1.基础/08_面向对象基础/模块制作/setup.py
@@ -1,5 +1,5 @@
 ## 1.创建setup.py文件
-from setuptools import setup  # 改为从 setuptools 导入
+from setuptools import setup,find_packages  # 改为从 setuptools 导入
 
 setup(
     name="hm_message",
@@ -7,14 +7,12 @@ setup(
     description="模块描述xxx",
     long_description="长描述xx",
     author="wm",
-    py_modules=["mypackage.ReceiveMessageUtil",
-                "mypackage.SendMessageUtil"]
+    packages=find_packages(),
 )
 
 
-## 2.构建模块 python3 setup.py build
-## 3.生成发布压缩包  python3 setup.py sdist
+## 2.构建模块      python3 setup.py build
+## 3.生成发布压缩包 python3 setup.py sdist
 ########=================================
 ##4.安装压缩包
-#     a. tar -zxvf hm_message-1.0.tar.gz
-#     b. python3 setup.py install
+#     pip3 install hm_message-1.0.tar.gz
