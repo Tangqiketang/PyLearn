dbb67e5104e5239f21dad68166342eb0e1b36937
wangmin <641718840@qq.com>
2026-02-04 23:44:24 +0800

    model

--- 23.深度学习/pytorch/LeNet/model.py
+++ 23.深度学习/pytorch/LeNet/model.py
@@ -6,10 +6,10 @@ class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         ##如果输入为28*28*1,则卷积核为 5*5*1*6
-        self.conv1 = nn.Conv2d(1, 6, 5, 2)
-        self.sigmoid = nn.Sigmoid()  ##sigmoid要几次？？？
+        self.conv1 = nn.Conv2d(1, 6, 5, 1,2)
+        self.sigmoid = nn.Sigmoid()  ##sigmoid每次卷积都需要
         self.avgPool1 = nn.AvgPool2d(2, 2)
-        self.conv3 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.avgPool2 = nn.AvgPool2d(2, 2)
 
         self.flatten = nn.Flatten()
@@ -17,3 +17,25 @@ class LeNet(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.sigmoid(x)
+        x = self.avgPool1(x)
+
+        x = self.conv2(x)
+        x = self.sigmoid(x)
+        x = self.avgPool2(x)
+
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = LeNet().to(device)
+    ##输入图像为28*28*1。  参数个数=输出通道数*（输入通道*卷积核高*卷积核宽+1）=6*（1*5*5+1）。这里的+1是因为偏置个数
+    summary(model, (1, 28, 28))
